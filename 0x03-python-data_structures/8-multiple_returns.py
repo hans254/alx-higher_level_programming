762bbcdeb90615a97962a84ffcd725556871e17c
hans254 <hansfidel6@gmail.com>
2023-06-11 02:29:18 -0700

    Write a function that returns a tuple with the length of a string and its first character.

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    summation = len(sentence)
+    char = sentence[0] if total > 0 else "None"
+    new-summation = summation, char
+    return(new-summation)
