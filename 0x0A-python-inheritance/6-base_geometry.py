dc39957b19a62c746ad48718e1d714ad9ffd351c
hans254 <hansfidel6@gmail.com>
2023-07-10 07:04:38 -0700

    Write a class BaseGeometry (based on 5-base_geometry.py).:

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+#Author: Hansel Fidel Ndemange
+"""
+===================================
+module with class BaseGeometry
+===================================
+"""
+
+
+class BaseGeometry:
+    """BaseGeometry class"""
+
+    @classmethod
+    def area(self):
+        """method for calculated area"""
+
+        raise Exception("area() is not implemented")
+
