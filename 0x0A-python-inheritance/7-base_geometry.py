5e680a6826da925946768af73bc0f9995404a072
hans254 <hansfidel6@gmail.com>
2023-07-10 07:39:46 -0700

    Write a class BaseGeometry (based on 6-base_geometry.py).

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,24 +1,19 @@
 #!/usr/bin/python3
-#Author: Hansel Fidel Ndemange
-"""
-===================================
-module with class BaseGeometry
-===================================
-"""
+""" This module defines a class based on 6-base_geometry.py ."""
 
 
 class BaseGeometry:
-    """BaseGeometry class"""
+    """Base Geometry class"""
 
     def area(self):
-        """method for calculated area"""
+        """unimplemneted area method """
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """Method for validate if a num is integer"""
-
-        if type(value) is not int:
+        """validates value """
+        if type(value) != int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
+
 
